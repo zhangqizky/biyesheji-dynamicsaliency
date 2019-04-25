c639c5f87933a725b6dfe7f43e8242db3865c181
buaazhangqi <2445657635@qq.com>
2019-04-25 16:15:18 +0800

    Update K-MeanswithOpticalflow.cpp

--- K-MeanswithOpticalflow.cpp
+++ K-MeanswithOpticalflow.cpp
@@ -29,17 +29,6 @@ using namespace cv;
 
 
 
-// Farneback dense optical flow calculate and show in Munsell system of colors  
-// Author : Zouxy  
-// Date   : 2013-3-15  
-// HomePage : http://blog.csdn.net/zouxy09  
-// Email  : zouxy09@qq.com  
-
-// API calcOpticalFlowFarneback() comes from OpenCV, and this  
-// 2D dense optical flow algorithm from the following paper:  
-// Gunnar Farneback. "Two-Frame Motion Estimation Based on Polynomial Expansion".  
-// And the OpenCV source code locate in ..\opencv2.4.3\modules\video\src\optflowgf.cpp  
-
 #include <iostream>  
 #include "opencv2/opencv.hpp"  
 
